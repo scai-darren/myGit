bab39686db8e70da6feaf5cecd6e7a5c805c2822
scai-darren <44920812+scai-darren@users.noreply.github.com>
2018-11-19 15:58:18 +0800

    drools

--- Test2/src/test/java/test.java
+++ Test2/src/test/java/test.java
@@ -2,9 +2,9 @@
 public class test {
 
 	public static void main(String[] args) {
-		String value = "drl";
-		if(value.equals("drl")){
-			System.out.println("drl");
+		String value = "drools";
+		if(value.equals("drools")){
+			System.out.println("drools");
 		}
 	}
 
