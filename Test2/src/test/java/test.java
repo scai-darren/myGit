79f21bbf38fb8cfdd2c490bd2e4bc8c20349d744
scai-darren <44920812+scai-darren@users.noreply.github.com>
2018-11-19 15:56:36 +0800

    masterI

--- Test2/src/test/java/test.java
+++ Test2/src/test/java/test.java
@@ -4,10 +4,7 @@ public class test {
 	public static void main(String[] args) {
 		int i = 0;
 		System.out.println(i);
-		String value = "drl";
-		if(value.equals("drl")){
-			System.out.println("drl");
-		}
+	
 	}
 
 }
