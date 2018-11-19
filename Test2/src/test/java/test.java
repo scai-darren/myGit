b4b5f037c997173e09f56d5d818114e1715e1f6d
scai-darren <ylxxynlcg@163.com>
2018-11-19 15:50:23 +0800

    drl

--- Test2/src/test/java/test.java
+++ Test2/src/test/java/test.java
@@ -2,8 +2,10 @@
 public class test {
 
 	public static void main(String[] args) {
-		int i = 0;
-		System.out.println(i);
+		String value = "drl";
+		if(value.equals("drl")){
+			System.out.println("drl");
+		}
 	}
 
 }
